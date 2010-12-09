22f7724eb87ec007fbf3d53718fadd5140b0f10d
Chris Richards <chris.richards@yellowfeather.co.uk>
2010-12-09 23:34:45 +0000

    Generated the Rails 3 application

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '2e463a5e9764367d5574902f7ce1029bb66d8841c7e8c1fececaf7898bb7e82548a5bac1ce6e1d6fdf88fc0abf01ffc72978094e873a73b7e3f86a236e46912e'
