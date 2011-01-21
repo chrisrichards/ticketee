3d3794c8071050d423477aec2cd3ede874bb67c8
Chris Richards <chris.richards@yellowfeather.co.uk>
2011-01-21 23:28:52 +0000

    Implemented features for displaying a list of tickets for a project.

--- ticketee/features/support/env.rb
+++ ticketee/features/support/env.rb
@@ -15,6 +15,9 @@ require 'cucumber/web/tableish'
 require 'capybara/rails'
 require 'capybara/cucumber'
 require 'capybara/session'
+
+Capybara.default_driver = :rack_test
+
 # require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
